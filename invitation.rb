0038fea31f7cf3e5f0cfd821ddb2cd2247ee0fa8
MCPhillips19 <mpclaire72@gmail.com>
2018-07-24 19:43:29 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,6 +1,13 @@
 # Code your prompts here!
 
-def invitation(your_name, )
+def invitation(guest_name, party_name, date, time, host_name)
+  puts "Dear #{guest_name},"
+  
+  puts "you are cordially invited to #{party_name} on #{date} at #{time}. Please RSVP no later than #{date}."
+  
+  
+  puts "Sincerely,
+  #{host_name}"
   
 
 
